91474ec7f63ff4c9849439634145dfad6a86042e
shulme801 <shulme801@gmail.com>
2023-07-27 11:51:48 -0400

    27 July MinMax Challenge refactored

--- MinMaxChallenge/src/MinMaxChallenge.java
+++ MinMaxChallenge/src/MinMaxChallenge.java
@@ -5,26 +5,29 @@ public class MinMaxChallenge {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double min = Double.MIN_VALUE;
-        double max = Double.MAX_VALUE;
-        boolean done = false;
+        double min = 0;
+        double max = 0;
+        int loopCount = 0;
 
 
-        while (! done) {
-            System.out.println("Enter a number or a non-digit character to quit"  + ":");
-            String nextNumber = scanner.nextLine();
-            try {
-                double number = Double.parseDouble(nextNumber);
-                if (number < min) {
-                    min = number;
-                } else if (number > max) {
-                    max = number;
+        do {
+                System.out.println("Enter a number or a non-digit character to quit"  + ":");
+                String nextInput = scanner.nextLine();
+                try {
+                    double validNum = Double.parseDouble(nextInput);
+                    if ((loopCount == 0) || (validNum < min)) {
+                      min = validNum;
+                    }
+                    if ((loopCount == 0) || (validNum >max)) {
+                      max = validNum;
+                    }
+                    loopCount++;
+                } catch (NumberFormatException nfe){
+                    System.out.println("Non-numeric character entered, so quitting");
+                        break;
                 }
-            } catch (NumberFormatException nfe){
-                System.out.println("Non-numeric character entered, so quitting");
-                done = true;
-            }
-        }
+        } while (true);
+
         System.out.println("Minimum number entered is: " + min);
         System.out.println("Maximum number entered is: " + max);
     }
