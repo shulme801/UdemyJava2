184393d6ac92dfe4b6b2a31549842e0fe52753ef
shulme801 <shulme801@gmail.com>
2023-07-28 16:53:40 -0400

    28 July Refactored SumAndAverage.java

--- SumAndAverage/src/SumAndAverage.java
+++ SumAndAverage/src/SumAndAverage.java
@@ -12,30 +12,22 @@ public class SumAndAverage {
         int sum = 0;
         int counter = 0;
 
-        do {
-            System.out.println("Enter an integer or a non-integer to quit: ");
+        while(true) {
+            System.out.println("Enter a number to sum and average, or a non-number to quit:");
             String nextNumber = scanner.nextLine();
             try {
-            } catch (NumberFormatException nfe) {
-
-            }while (true);
-
-           String input = scanner.nextLine();
-
-            if (input) {
-                int inputInt = scanner.parse();
-                sum += inputInt;
+                int number = Integer.parseInt(nextNumber);
+                sum += number;
                 counter++;
-            }
-            else {
+            } catch (NumberFormatException nfe){
+                System.out.println("Invalid number entered -- exiting");
                 double average = (double) sum / counter;
                 System.out.println("SUM = " + sum + " AVG = " + Math.round(average));
                 break;
             }
-            scanner.nextLine();
         }
-        scanner.close();
     }
+
 
 
 }
