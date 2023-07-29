b6498fd8b9a964b85584ab479be9aa6141936a96
shulme801 <shulme801@gmail.com>
2023-07-29 14:58:15 -0400

    29 July ClassesChallenge commit 2

--- ClassesChallenge/src/Account.java
+++ ClassesChallenge/src/Account.java
@@ -1,2 +1,64 @@
 public class Account {
+
+    private String accountNumber;
+    private double accountBalance;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhoneNumber;
+
+    public void depositFunds(double depositAmount) {
+        this.accountBalance += depositAmount;
+        System.out.println("Deposit of $" + depositAmount + " made. New balance is $" + this.accountBalance);
+    }
+
+    public void withdrawFunds(double withdrawalAmount) {
+        if (this.accountBalance - withdrawalAmount < 0) {
+            System.out.println("Current Balance is $" + this.accountBalance
+                    + ". You have insufficient funds for a withdrawal of $"
+                    + withdrawalAmount);
+        } else {
+            this.accountBalance -= withdrawalAmount;
+            System.out.println("After withdrawal of $" + withdrawalAmount + " your new balance is $"
+                                + this.accountBalance);
+        }
+    }
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
 }
--- ClassesChallenge/src/Main.java
+++ ClassesChallenge/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        Account myAccount = new Account();
+
+        myAccount.setAccountBalance(1000.00);
+        myAccount.setAccountNumber("123456");
+        myAccount.setCustomerName("Sam SelfReferential");
+        myAccount.setCustomerEmail("myemail@selfreferential.com");
+        myAccount.setCustomerPhoneNumber("919-555-1212");
+
+        myAccount.depositFunds(250);
+        myAccount.withdrawFunds(100.00);
+        myAccount.withdrawFunds(150);
+        myAccount.depositFunds(100.00);
+        myAccount.withdrawFunds(45.55);
+        myAccount.withdrawFunds(54.46);
+        myAccount.withdrawFunds(54.45);
 
     }
 }
