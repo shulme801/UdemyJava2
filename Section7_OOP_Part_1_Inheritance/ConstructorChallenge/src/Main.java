edde3f5d66db2fe0ef04c194550a166ac5b3a545
Stephen H. Hulme <shulme801@gmail.com>
2023-08-01 10:41:44 -0400

    01 August Constructor Challenge Commit 2

--- Section7_OOP_Part_1_Inheritance/ConstructorChallenge/src/Main.java
+++ Section7_OOP_Part_1_Inheritance/ConstructorChallenge/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        Customer customer = new Customer("Tim", 1000,
-                "tim@email.com");
+        Customer customer = new Customer("Stephen", 1000,
+                "stephen@email.com");
         System.out.println(customer.getName());
         System.out.println(customer.getCreditLimit());
         System.out.println(customer.getEmail());
