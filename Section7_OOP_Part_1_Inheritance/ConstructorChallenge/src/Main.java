f6f09a78517f1b9500ef8f520dcf935f51b2fc69
Stephen H. Hulme <shulme801@gmail.com>
2023-08-01 10:52:13 -0400

    01 August Deleted junk from IDEA

--- Section7_OOP_Part_1_Inheritance/ConstructorChallenge/src/Main.java
+++ Section7_OOP_Part_1_Inheritance/ConstructorChallenge/src/Main.java
@@ -16,5 +16,6 @@ public class Main {
         System.out.println(thirdCustomer.getName());
         System.out.println(thirdCustomer.getCreditLimit());
         System.out.println(thirdCustomer.getEmail());
+
     }
 }
