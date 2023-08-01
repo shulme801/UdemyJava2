2f4a52d9f9cc352f1fab45630d110795b98e60f9
Stephen H. Hulme <shulme801@gmail.com>
2023-08-01 10:14:31 -0400

    1 August Customer Challenge commit 1

---
+++ src/Customer.java
@@ -0,0 +1,24 @@
+public class Customer {
+
+    private String customerName;
+    private double creditLimit;
+    private String customerEmail;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public Customer(String customerName, double creditLimit, String customerEmail) {
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.creditLimit = creditLimit;
+    }
+}
