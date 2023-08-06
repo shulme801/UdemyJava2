c551a85adba5de59d7a1276a650532bec5b041d2
Stephen H. Hulme <shulme801@gmail.com>
2023-08-06 16:47:51 -0400

    6 August Inheritance Challenge Commit 2

--- Section7_OOP_Part_1_Inheritance/InheritanceChallenge/src/Main.java
+++ Section7_OOP_Part_1_Inheritance/InheritanceChallenge/src/Main.java
@@ -6,10 +6,13 @@ public class Main {
         System.out.println("Age = " + stephen.getAge());
         System.out.println("Pay = " + stephen.collectPay());
 
-        Employee fred = new Employee("Fred Flintstone", "10/11/1952", "01/03/2021");
+        SalariedEmployee fred = new SalariedEmployee("Fred Flintstone", "10/11/1952", "01/03/2021", 35000);
         System.out.println(fred);
         System.out.println("Age = " + fred.getAge());
-        System.out.println("Pay = " + fred.collectPay());
+        System.out.println("Fred's Paycheck = " + fred.collectPay());
+
+        fred.retire();
+        System.out.println("Fred's Pension Check = $" + fred.collectPay());
 
 
     }
