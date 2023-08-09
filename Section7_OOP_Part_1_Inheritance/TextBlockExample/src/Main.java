385d01ed079d420cd729bd241553b021b0258dbc
Stephen H. Hulme <shulme801@gmail.com>
2023-08-09 08:38:33 -0400

    9 August Text Block Example Commit 3

--- Section7_OOP_Part_1_Inheritance/TextBlockExample/src/Main.java
+++ Section7_OOP_Part_1_Inheritance/TextBlockExample/src/Main.java
@@ -9,5 +9,21 @@ public class Main {
                 \t\t• Sub Point
                 """;
         System.out.println(textBlockExample);
+
+        int age = 35;
+        System.out.printf("Your age is %d%n", age); // %d is a "format specifier"
+
+        int yearOfBirth = 2023 - age;
+        System.out.printf("Age = %d, Year of Birth = %d%n", age, yearOfBirth);
+
+        System.out.printf("Your floating number age is %.2f%n", (float) age);
+
+        for (int i = 1; i <= 100000; i *= 10) {
+            System.out.printf("Printing %.2f %n", (float) i);
+        }
+
+        for (int i = 1; i <= 1000000; i *= 10) {
+            System.out.printf("Printing %6d %n", i);
+        }
     }
 }
