7f283d35c40f21f295a2e20d740abf24bf37dd96
Stephen H. Hulme <shulme801@gmail.com>
2023-08-09 08:08:08 -0400

    9 August Text Block Example Commit 1

---
+++ Section7_OOP_Part_1_Inheritance/TextBlockExample/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String bulletIt = "Print a Bulleted List:\n" +
+                "\t\u2022 First Point\n" +
+                "\t\t\u2022 Sub Point";
+    }
+}
