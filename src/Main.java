a1a3255b65de0019343da18a46f4a6c30084247d
Stephen H. Hulme <shulme801@gmail.com>
2023-08-18 15:11:26 -0500

    18 August SmartKitchen2 fixed calls to setKitchenState

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        SmartKitchen kitchen = new SmartKitchen();
+
+//        kitchen.getOxo().setHasWorkToDo(true);
+//        kitchen.getKitchenAid().setHasWorkToDo(false);
+//        kitchen.getBosch().setHasWorkToDo(true);
+//
+//        kitchen.getBosch().WashDishes();
+//        kitchen.getKitchenAid().orderFood();
+//        kitchen.getOxo().brewCoffee();
+//
+
+        kitchen.setKitchenState(true, false, true);
+        kitchen.doKitchenWork();
     }
 }
--- src/SmartKitchen.java
+++ src/SmartKitchen.java
@@ -1,7 +1,40 @@
 public class SmartKitchen {
     private CoffeeMaker oxo;
     private Refrigerator kitchenAid;
-    private Dishwaher bosch;
+    private DishWasher bosch;
+
+    public SmartKitchen() {
+        oxo = new CoffeeMaker();
+        kitchenAid = new Refrigerator();
+        bosch = new DishWasher();
+    }
+
+    public CoffeeMaker getOxo() {
+        return oxo;
+    }
+
+    public Refrigerator getKitchenAid() {
+        return kitchenAid;
+    }
+
+    public void setKitchenState(boolean coffeeFlag, boolean fridgeFlag, boolean dishwasherFlag) {
+
+        oxo.setHasWorkToDo(coffeeFlag);
+        bosch.setHasWorkToDo(fridgeFlag);
+        kitchenAid.setHasWorkToDo(dishwasherFlag);
+
+
+    }
+
+    public void doKitchenWork() {
+        oxo.brewCoffee();
+        kitchenAid.orderFood();
+        bosch.WashDishes();
+    }
+
+    public DishWasher getBosch() {
+        return bosch;
+    }
 }
 
 class CoffeeMaker {
@@ -32,6 +65,23 @@ class Refrigerator {
 
         if (hasWorkToDo) {
             System.out.println("Ordering Food!!");
+            hasWorkToDo = false;
+        }
+    }
+
+}
+
+class DishWasher {
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void WashDishes() {
+
+        if (hasWorkToDo) {
+            System.out.println("Washing Dishes!!");
             hasWorkToDo = false;
         }
     }
