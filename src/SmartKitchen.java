67c168ab18ca61fe196e125fa2eed6587757049a
Stephen H. Hulme <shulme801@gmail.com>
2023-08-18 14:23:10 -0500

    18 August SmartKitchen2 commit 5

--- src/SmartKitchen.java
+++ src/SmartKitchen.java
@@ -16,7 +16,23 @@ class CoffeeMaker {
         if (hasWorkToDo) {
             System.out.println("Brewing Coffee!!");
             hasWorkToDo = false;
+        }
+    }
 
+}
+
+class Refrigerator {
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void orderFood() {
+
+        if (hasWorkToDo) {
+            System.out.println("Ordering Food!!");
+            hasWorkToDo = false;
         }
     }
 
