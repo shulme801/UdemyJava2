4195ced6bbd7eb550c1ba7be51c269a14923db17
Stephen H. Hulme <shulme801@gmail.com>
2023-08-19 10:22:06 -0500

    19 August SmartKitchen2 added Microwave appliance to SmartKitchen.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@ public class Main {
     public static void main(String[] args) {
         SmartKitchen kitchen = new SmartKitchen();
 
-        kitchen.setKitchenState(false, true, true);
+        // Let's try out the microwave, using the overloaded setKitchenState method.
+        System.out.printf("%nAdded the %s appliance%n", "Old fashioned Amana RadarRange");
+        kitchen.setKitchenState(true, true, false, true);
         kitchen.doKitchenWork();
+
+        // Clean up the SmartKitchen using the overloaded, three appliance, setKitchenState.
+        kitchen.setKitchenState(false, false, true);
+        kitchen.doKitchenWork();
+
     }
 }
--- src/SmartKitchen.java
+++ src/SmartKitchen.java
@@ -2,24 +2,36 @@ public class SmartKitchen {
     // This challenge uses composition only.  Nothing is inherited from the parent class.  The advantage
     // is this: the calling code needs to know nothing about the details of the appliances in the
     // SmartKitchen.  An appliance can be added without changing the calling code in Main.java.
+
+    // Making these appliances into constants with the "final" keyword, since we never change their values.
     private final CoffeeMaker oxo;
     private final Refrigerator kitchenAid;
     private final DishWasher bosch;
+    private final Microwave radarRange;
 
     public SmartKitchen() {
         // Create the appliances
         oxo        = new CoffeeMaker();
         kitchenAid = new Refrigerator();
         bosch      = new DishWasher();
+        radarRange = new Microwave();
     }
 
 
     public void setKitchenState(boolean coffeeFlag, boolean fridgeFlag, boolean dishwasherFlag) {
         // See which appliances need to do the work.
         oxo.setHasWorkToDo(coffeeFlag);
-        bosch.setHasWorkToDo(fridgeFlag);
-        kitchenAid.setHasWorkToDo(dishwasherFlag);
+        bosch.setHasWorkToDo(dishwasherFlag);
+        kitchenAid.setHasWorkToDo(fridgeFlag);
 
+    }
+
+    public void setKitchenState(boolean coffeeFlag, boolean fridgeFlag, boolean dishwasherFlag, boolean microwaveFlag) {
+        // See which appliances need to do the work.
+        oxo.setHasWorkToDo(coffeeFlag);
+        bosch.setHasWorkToDo(dishwasherFlag);
+        kitchenAid.setHasWorkToDo(fridgeFlag);
+        radarRange.setHasWorkToDo(microwaveFlag);
 
     }
 
@@ -27,6 +39,7 @@ public class SmartKitchen {
         oxo.brewCoffee();
         kitchenAid.orderFood();
         bosch.WashDishes();
+        radarRange.cookFood();
     }
 
 }
@@ -76,6 +89,23 @@ class DishWasher {
 
         if (hasWorkToDo) {
             System.out.println("Washing Dishes!!");
+            hasWorkToDo = false;
+        }
+    }
+
+}
+
+class Microwave {
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void cookFood() {
+
+        if (hasWorkToDo) {
+            System.out.println("Cooking Food!!");
             hasWorkToDo = false;
         }
     }
